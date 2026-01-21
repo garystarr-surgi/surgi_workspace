be95fd7eb80bfb7ae78c5ac36828515f5a726a1d
garystarr-surgi <gary.starr@surgishop.com>
2026-01-21 16:14:05 -0500

    Update hooks.py

--- surgi_workspace/hooks.py
+++ surgi_workspace/hooks.py
@@ -1,1 +1,14 @@
+app_name = "surgi_workspace"
+app_title = "Surgi Workspace"
+app_publisher = "SurgiShop"
+app_description = "Create custom workspaces for users"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
+
+
+fixtures = [
+    {"doctype": "Workspace", "filters": [["name", "=", "Sales Workspace"]]}
+]
+
+app_include_js = "/assets/surgi_workspace/js/desk.js"
 
